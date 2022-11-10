2802e87bb27c88cf1355da485b20f6350561cfb2
Carlos Eugenio Torres <cetorresusa@gmail.com>
2022-11-09 19:21:33 -0700

    Fixed pq-linklist.c with priority now.

--- main_p1.c
+++ main_p1.c
@@ -3,12 +3,17 @@
 #include <time.h>
 
 #include "pq.h"
-// #include "m5ops.h"
 
 int main(int argc, char *argv[])
 {
+  // Check for n on arguments
+  if (argc < 2) {
+    printf("Please inform a number n\n");
+    return 1;
+  }
+
   int i;
-  const int n = 10000;
+  const int n = atoi(argv[1]); // 10000;
   double *v = malloc(sizeof(double)*n);
   double **p = malloc(sizeof(double*)*n);
   pq *mypq;
@@ -21,14 +26,10 @@ int main(int argc, char *argv[])
     p[i] = &v[i];
   }
 
-  // m5_dump_reset_stats()
-
   /* begin sort */
-  for (i = 0; i < n; i++) pq_push(mypq, v[i], (void*)p[i]);
-  for (i = 0; i < n; i++) p[i] = pq_pop(mypq);
+  for (i = 0; i < n; i++) pq_push(&mypq, v[i], (void*)p[i]);
+  for (i = 0; i < n; i++) p[i] = pq_pop(&mypq);
   /* end sort */
-
-  // m5_dump_reset_stats()
 
   for (i = 0; i < n; i++) {
     if (p[i]) printf("%g\n", *p[i]);
--- pq.h
+++ pq.h
@@ -5,13 +5,11 @@ typedef struct pq pq;
 pq* pq_create();
 
 /* Adds value to pq based on numerical order of key */
-void pq_push(pq *head, double key, void *value);
+void pq_push(pq **head, double key, void *value);
 
 /* Returns value from pq having the minimum key */
-void* pq_pop(pq *head);
+void* pq_pop(pq **head);
 
 /* Deallocates (frees) pq. Shallow destruction,
 meaning nodes in the pq are not recursively freed. */
 void pq_destroy();
-
-
