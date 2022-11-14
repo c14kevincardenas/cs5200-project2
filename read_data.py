d2b0bd93349d7c3450587dac6729f81caec3cb2c
c14kevincardenas <kevincardenas4@gmail.com>
2022-11-14 10:35:38 -0700

    moved p3a into default folder, updated data script, started report p1&2

--- read_data.py
+++ read_data.py
@@ -19,9 +19,17 @@ def get_data(filename):
     return seconds, l1d_miss, l1i_miss, l2_miss
 
 
-def get_inst_mix(filename, p):
-    inst_mix = {}
+def get_inst_mix(filename, p, pq, n):
+    # initialize dictionary with pq and n (part 2: include part of the code)
+    if p == 'p1':
+        inst_mix = {'pq': [pq],
+                    'n': [n]}
+    else:
+        inst_mix = {'pq': [pq, pq, pq],
+                    'n': [n, n, n],
+                    'part': ['a', 'b', 'c']}
     
+    # iterate through lines in file and add instruction counts > 0
     with open(filename) as f:
         lines = f.readlines()
         for line in lines:
@@ -31,15 +39,41 @@ def get_inst_mix(filename, p):
                 # check if line is class of exec inst
                 if contents[0].split('.')[-1].split('::')[0] == 'statExecutedInstType':
                     num_ops = int(contents[1])
-                    if num_ops > 0:
-                        op_type = contents[0].split('.')[-1].split('::')[-1]
+                    op_type = contents[0].split('.')[-1].split('::')[-1]
+                    if num_ops > 0 or op_type in inst_mix:
                         if op_type != 'total':
                             if op_type not in inst_mix:
                                 inst_mix[op_type] = [num_ops]
                             else:
                                 inst_mix[op_type].append(num_ops)
     
+    # ensure all ops have the 3 entries (part a b and c)
+    if p == 'p2':
+        for op, vals in inst_mix.items():
+            if len(vals) < 2:
+                vals.insert(0, 0)
+                vals.insert(0, 0)
+            elif len(vals) < 3:
+                vals.insert(0, 0)
+    
     return inst_mix
+
+
+def make_inst_mix_df(inst_mix):
+    # initialize 2 empty df
+    df1 = pd.DataFrame()
+    df2 = pd.DataFrame()
+    
+    # iterate through dictionaries to combine p1 dicts and p2 dicts
+    for mix in inst_mix:
+        # part 1
+        if len(mix['pq']) == 1:
+            df1 = pd.concat([df1, pd.DataFrame.from_dict(mix)])
+        else:  # part 2
+            df2 = pd.concat([df2, pd.DataFrame.from_dict(mix)])
+    
+    return df1, df2
+
 
 def fill_data(data, time, reads, writes, l1d_miss, l1i_miss, part, cpu, fu, op_lat, pq):
     data['time'].append(time)
@@ -55,11 +89,12 @@ def fill_data(data, time, reads, writes, l1d_miss, l1i_miss, part, cpu, fu, op_l
 
 
 # function to compile the data from part folders into a dictionary and return a dataframe
-def make_df():
+def read_data():
     # init parts of file structure
     pqs = ['linklist', 'minheap']
-    parts = ['p1', 'p2', 'p3', 'p4', 'p5']
-    fus = ['base', '6_1', '5_2', '4_3', '3_4', '2_5', '1_6']
+    parts = ['p1', 'p2']
+    # parts = ['p1', 'p2', 'p3', 'p4', 'p5']
+    fus = ['default', '6_1', '5_2', '4_3', '3_4', '2_5', '1_6']
     op_lats = ['2_4', '1_4', '2_2']
     
     # data dictionary for p3-p5
@@ -84,57 +119,45 @@ def make_df():
         for p in parts:
             # construct the filename, get the time, fill the dictionary 'data'
             if p == 'p1':
-                filename = 'output/' + p + '/' + pq + '-stats.txt'
-                print(filename)
-                # inst_mix.append(get_inst_mix('m5out/p1/linklist-stats.txt', p))
+                filename = 'm5out/' + p + '/' + pq + '-stats.txt'
+                inst_mix.append(get_inst_mix(filename, p, pq, 10000))
             elif p == 'p2':
                 for n in [1000, 10000]:
-                    filename = 'output/' + p + '/' + pq + '-n' + str(n) + '-stats.txt'
-                    print(filename)
-                    # inst_mix.append(get_inst_mix('m5out/p2/linklist-n1000-stats.txt', p))
+                    filename = 'm5out/' + p + '/' + pq + '-n' + str(n) + '-stats.txt'
+                    inst_mix.append(get_inst_mix(filename, p, pq, n))
             elif p == 'p3':
                 for fu in fus:
                     if fu == 'base':
                         for n in [10, 1000, 10000]:
-                           filename = 'output/' + p + '/'  + fu + '/' + pq + '-n' + str(n) + '-stats.txt'
+                           filename = 'm5out/' + p + '/'  + fu + '/' + pq + '-n' + str(n) + '-stats.txt'
                            print(filename)
                     else:
-                        filename = 'output/' + p + '/' + fu + '/' + pq + '-stats.txt'
+                        filename = 'm5out/' + p + '/' + fu + '/' + pq + '-stats.txt'
                         print(filename)
             
             elif p == 'p4':
                 for op_lat in op_lats:
-                    filename = 'output/' + p + '/' + op_lat + '/' + pq + '-stats.txt'
+                    filename = 'm5out/' + p + '/' + op_lat + '/' + pq + '-stats.txt'
                     print(filename)
             
             else:
                 for combo in fus + op_lats:
-                    filename = 'output/' + p + '/' + combo + '/' + pq + '-stats.txt'
+                    filename = 'm5out/' + p + '/' + combo + '/' + pq + '-stats.txt'
                     print(filename)
-
-            
     
-    # make fake data
-    for key in data:
-        if key == 'fu':
-            data[key] = [random.choice(fus) for i in range(100)]
-        elif key == 'op_lat':
-            data[key] = [random.choice(op_lats) for i in range(100)]
-        elif key == 'part':
-            data[key] = [random.choice(parts) for i in range(100)]
-        elif key == 'pq':
-            data[key] = [random.choice(['linklist', 'minheap']) for i in range(100)]
-        else: 
-            data[key] = [random.randint(0,100) for i in range(100)]
     
+    # make dataframes for p1 and p2
+    df1, df2 = make_inst_mix_df(inst_mix)
+    df1 = df1.melt(id_vars=['pq', 'n'], var_name='op_class')
+    df2 = df2.melt(id_vars=['pq', 'n', 'part'], var_name='op_class')
     
     # make pandas dataframe
-    df = pd.DataFrame.from_dict(data)
+    # df = pd.DataFrame.from_dict(data)
     # df.cpu = df.fus.map({'6_1':'6 IssueLat, 1 OpLat'})
 
 		
-    return df
+    return df1, df2
 
 if __name__ == '__main__':
-    make_df()
+    df1, df2 = read_data()
     # inst_mix = get_inst_mix('m5out/p1/linklist-stats.txt', 'p1')
